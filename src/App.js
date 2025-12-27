90b8c7d650e4bcb63e63a54f0759a96264278123
c-jeremy <czm@czming.top>
2025-12-27 19:01:25 +0800

    Claude did some chores to make it React.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/styles/variables.css';
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Main application content */}
+    </div>
+  );
+}
+
+export default App;
